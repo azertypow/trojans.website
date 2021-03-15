dae54b25a729ffd748fe5da4ccd6ad03cf10dbc5
azertypow <azertypow@gmail.com>
2021-03-15 21:59:57 +0100

    build: v0.1.1

---
+++ docs/precache-manifest.5bf4a4a7d0292058a82f2679b620b447.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e493264ccab79088e18",
+    "url": "/css/ViewProjects.187aef2c.css"
+  },
+  {
+    "revision": "9f098765845ff40b9fb2",
+    "url": "/css/app.c4191da3.css"
+  },
+  {
+    "revision": "b9eff66e84f7e3d0226adb81121c779e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "031df5dd4d515eb2b2be",
+    "url": "/js/ViewHome.27042a75.js"
+  },
+  {
+    "revision": "5e493264ccab79088e18",
+    "url": "/js/ViewProjects.fa528419.js"
+  },
+  {
+    "revision": "9f098765845ff40b9fb2",
+    "url": "/js/app.051118d0.js"
+  },
+  {
+    "revision": "7d876b52ebe072ab1928",
+    "url": "/js/chunk-vendors.d6c9b4d4.js"
+  },
+  {
+    "revision": "db31bf4807f47c668280a84a26a5bfcf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
