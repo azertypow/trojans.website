b5df98adc7e05fe51976b185df2ddc9b213fb7c5
azertypow <azertypow@gmail.com>
2021-07-13 19:41:31 +0200

    fix: remove https settings url

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@
 module.exports = {
 
     publicPath: process.env.NODE_ENV === 'production'
-        ? 'https://trojanscollective.com/'
+        ? 'http://trojanscollective.com/'
         : '/',
     pwa: {
         themeColor: '#00EF2E',
