4d68a86e86f14460c23ae3f3bf8a58dd1a04dfcb
azertypow <azertypow@gmail.com>
2021-08-11 16:01:34 +0200

    feat: init scroll position

--- src/components/ToggleTable.vue
+++ src/components/ToggleTable.vue
@@ -128,10 +128,12 @@ export default defineComponent({
       console.log("document.getElementsByClassName(\"v-view-projects\")[0].scrollLeft", document.getElementsByClassName("v-view-projects")[0].scrollLeft)
       console.log("(this.$refs.headerContainer as HTMLElement).getBoundingClientRect().left", (this.$refs.headerContainer as HTMLElement).getBoundingClientRect().left)
 
-      document.getElementsByClassName("v-view-projects")[0].scroll({
-        top: 0,
-        left: document.getElementsByClassName("v-view-projects")[0].scrollLeft + (this.$refs.headerContainer as HTMLElement).getBoundingClientRect().x,
-      })
+      window.setTimeout(()=>{
+        document.getElementsByClassName("v-view-projects")[0].scroll({
+          top: 0,
+          left: document.getElementsByClassName("v-view-projects")[0].scrollLeft + (this.$refs.headerContainer as HTMLElement).getBoundingClientRect().x,
+        })
+      }, 150)
     },
   },
 
