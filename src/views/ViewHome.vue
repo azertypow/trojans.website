83509083fb84de8fa000d0674a67c1c63f5d0042
azertypow <azertypow@gmail.com>
2021-09-15 13:22:17 +0200

    fix: home img size

--- src/views/ViewHome.vue
+++ src/views/ViewHome.vue
@@ -114,9 +114,9 @@ export default defineComponent({
   }
 
   > .v-gallery {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
+    width: 100% !important;
+    height: 100% !important;
+    object-fit: cover !important;
   }
 }
 
